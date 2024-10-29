8c9b7f50d0366b98d5f934f9c3d4f2a2fb6d86fa
iiiSunshine <1840784717@qq.com>
2024-10-29 20:03:12 +0800

    Update hu.py

--- hu.py
+++ hu.py
@@ -12,8 +12,7 @@ with c2:
     if flag2:
         st.switch_page("pages/demo2.py")
 with c3:
-     st.image("OlP.jpg",use_column_width=True)
-    flag3=st.button("小白",use_container_width=True)
+    flag3=st.button("小白")
     if flag3:
         st.switch_page("pages/demo1.py")
 with c4:
