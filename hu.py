b574d1862e69f7803b6f68f0a7bcc191f53504f6
iiiSunshine <1840784717@qq.com>
2024-10-29 19:55:31 +0800

    Update hu.py

--- hu.py
+++ hu.py
@@ -10,16 +10,16 @@ with c2:
     st.image("https://pic2.zhimg.com/v2-e9fe02911227e55327de9de4572c1221_r.jpg",use_column_width=True)
     flag2 = st.button("花花",use_container_width=True)
     if flag2:
-        st.switch_page("pages/demo1.py")
+        st.switch_page("pages/demo2.py")
 with c3:
     flag3=st.button("咪咪",use_container_width=True)
     if flag3:
-        st.switch_page("pages/demo1.py")
+        st.switch_page("pages/demo3.py")
 with c4:
      st.image("https://img1.baidu.com/it/u=1493092819,2012763429&fm=253&fmt=auto&app=120&f=JPEG?w=800&h=800",use_column_width=True)
     flag4=st.button("美美",use_container_width=True)
     if flag4:
-        st.switch_page("pages/demo1.py")
+        st.switch_page("pages/demo4.py")
 with c4:
      st.image("https://img2.baidu.com/it/u=3086518359,3242772898&fm=253&fmt=auto&app=120&f=JPEG?w=500&h=667",use_column_width=True)
     flag4=st.button("小黑",use_container_width=True)
