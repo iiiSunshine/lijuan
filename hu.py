3ce1df5d9e13ac157752155b73a6bd2780a78dc4
iiiSunshine <1840784717@qq.com>
2024-10-29 19:45:15 +0800

    Update hu.py

--- hu.py
+++ hu.py
@@ -3,22 +3,23 @@ st.title("Chatbot")
 c1,c2,c3,c4=st.columns(4)
 with c1:
     st.image("download.jpg",use_column_width=True)
-    flag1 = st.button("快来和我聊天",use_container_width=True)
+    flag1 = st.button("我是你的宝贝",use_container_width=True)
     if flag1:
         st.switch_page("pages/demo1.py")
 with c2:
-    flag2 = st.button("进阶1")
+    st.image("https://iknow-pic.cdn.bcebos.com/10dfa9ec8a136327c1fd81ae838fa0ec08fac71f",use_column_width=True)
+    flag2 = st.button("花花",use_container_width=True)
     if flag2:
         st.switch_page("pages/demo1.py")
 with c3:
-    flag3=st.button("进阶2")
+    flag3=st.button("咪咪")
     if flag3:
         st.switch_page("pages/demo1.py")
 with c4:
-    flag4=st.button("进阶3")
+    flag4=st.button("美美")
     if flag4:
         st.switch_page("pages/demo1.py")
 with c4:
-    flag4=st.button("图片")
+    flag4=st.button("小黑")
     if flag4:
         st.switch_page("pages/image.py")
