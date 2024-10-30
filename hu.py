ab6fd37ae7d8c4b1db6cb9f414e157fc368ce1a5
iiiSunshine <1840784717@qq.com>
2024-10-30 09:35:18 +0800

    Update hu.py

--- hu.py
+++ hu.py
@@ -2,12 +2,12 @@ import streamlit as  st
 st.title("Chatbot")
 c1,c2,c3,c4=st.columns(4)
 with c1:
-    st.image("https://c-ssl.duitang.com/uploads/blog/202205/21/20220521115435_58029.jpg.jpg",use_column_width=True)
+    st.image("https://c-ssl.duitang.com/uploads/blog/202204/02/20220402195223_815f6.jpg",use_column_width=True)
     flag1 = st.button("我是你的宝贝",use_container_width=True)
     if flag1:
         st.switch_page("pages/demo1.py")
 with c2:
-    st.image("https://c-ssl.duitang.com/uploads/item/202007/28/20200728121207_cnsew.jpg",use_column_width=True)
+    st.image("https://th.bing.com/th/id/OIP.Rhil-o1qJ42UjFWcTziO7gAAAA?w=400&h=400&rs=1&pid=ImgDetMain",use_column_width=True)
     flag2 = st.button("花花",use_container_width=True)
     if flag2:
         st.switch_page("pages/demo2.py")
